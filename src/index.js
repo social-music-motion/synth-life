4296e9c34d68c9df53e6786276a82e5874d63670
Jeb Stone <jeb.b.stone@gmail.com>
2023-06-15 13:53:31 -0700

    Jeb Ian, switch to create-react-app base

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { ReactDOM } from 'react';
-// import App from './App.jsx';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-  <h1>hey</h1>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
